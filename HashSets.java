19a0af5a4f3de0923599580e003491a92d062f7c
karthhiik <karthikthatikonda18@gmail.com>
2024-08-29 18:55:26 +0530

    Update HashSets.java

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,35 @@
 public class HashSets{
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+  import java.util.*;
+
+class Person {
+    String name;
+    int n;
+
+    Person(String name, int n) {
+        this.name = name;
+        this.n = n;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", n=" + n + "]";
+    }
+
+}
+
+public class HashSets {
+    // Prepare Hashset object here and add person class objects in them iterate it
+    // by using for each
+    public static void main(String[] args) {
+        HashSet<Person> person = new HashSet<Person>();
+        person.add(new Person("John", 25));
+        person.add(new Person("John", 25));
+        person.add(new Person("hello", 34));
+
+        for (Person p : person) {
+            System.out.println(p);
+        }
+    }
+}
 }
